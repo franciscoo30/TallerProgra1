17affd5ab433be110de2f6584e6fe0ebfd89b1ac
diagnostico <f.barrozo02@ufromail.cl>
2023-08-24 16:38:38 -0400

    cuarto commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        crearMatrizCartas();
-        agregarCartas(matrizCartas, "A", "11");
         menu();
     }
     public static void menu(){
+        String [][] matrizCartas = crearMatrizCartas();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             imprimir();
@@ -14,8 +13,8 @@ public class Main {
             int opcion = scanner.nextInt();
 
             if (opcion == 1){
-                System.out.println("Seleccionó la opción 1");
-                break;
+                System.out.println("Seleccionó inicializar el juego");
+                inicializarCartasJuego(matrizCartas);
             } else if (opcion == 2){
                 System.out.println("Seleccionó la opción 2");
                 break;
@@ -30,7 +29,7 @@ public class Main {
     }
     public static void imprimir(){
         System.out.println("---Menú---");
-        System.out.println("1.- Opción 1");
+        System.out.println("1.- Inicializar el juego");
         System.out.println("2.- Opción 2");
         System.out.println("3.- Salir");
         System.out.println("Seleccione una opción");
@@ -38,10 +37,38 @@ public class Main {
     public static String [][] crearMatrizCartas(){
         String [][] matrizCartas = new String [12][2];
     return matrizCartas;}
-    public static void agregarCartas(String[][] matriz, String nombreCarta, String puntaje){
-        for (int columna = 0; columna < 12; columna++){
-            matriz[0][columna] = nombreCarta;
-            matriz[1][columna] = puntaje;
+    public static void agregarCartas(String[][] matriz, String nombreCarta, String puntaje) {
+        int numCartas = contarCartas(matriz);
+
+        if (numCartas < matriz.length) {
+            matriz[numCartas][0] = nombreCarta;
+            matriz[numCartas][1] = puntaje;
+        } else {
+            System.out.println("La matriz de cartas está llena, no se pueden agregar más cartas.");
         }
     }
+    public static int contarCartas(String[][] matriz) {
+        int contador = 0;
+        for (int fila = 0; fila < matriz.length; fila++) {
+            if (matriz[fila][0] != null) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public static void inicializarCartasJuego(String[][] matriz){
+        String[] nombresCartas = {"A","2","3","4","5","6","7","8","9","10","Q","J","K"};
+        String[] valoresCartas = {"11","2","3","4","5","6","7","8","9","10","10","10","10"};
+
+        for (int i = 0; i < matriz.length; i++){
+            matriz[i][0] = nombresCartas[i];
+            matriz[i][1] = valoresCartas[i];
+        }
+    }
+    public static String [][] obtenerCartas() {
+        String[][] matriz = crearMatrizCartas();
+        inicializarCartasJuego(matriz);
+        String[] jugador1 = {};
+        String[] jugador2 = {};
+    return matriz;}
 }
